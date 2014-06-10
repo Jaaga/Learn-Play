cece45dc06a8acd37ea0b8fe5ca0ddf0cbf151ea
Arpit <arpitagrawal910@gmail.com>
2014-06-10 17:17:37 +0530

    changes in display

--- gen/com/jaaga/learnnplay/R.java
+++ gen/com/jaaga/learnnplay/R.java
@@ -26,8 +26,58 @@ public final class R {
     public static final class drawable {
         public static final int a=0x7f020000;
         public static final int apple=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
-        public static final int rounded=0x7f020003;
+        public static final int b=0x7f020002;
+        public static final int ball=0x7f020003;
+        public static final int c=0x7f020004;
+        public static final int cat=0x7f020005;
+        public static final int d=0x7f020006;
+        public static final int dog=0x7f020007;
+        public static final int e=0x7f020008;
+        public static final int elephant=0x7f020009;
+        public static final int f=0x7f02000a;
+        public static final int fan=0x7f02000b;
+        public static final int g=0x7f02000c;
+        public static final int goat=0x7f02000d;
+        public static final int h=0x7f02000e;
+        public static final int hen=0x7f02000f;
+        public static final int i=0x7f020010;
+        public static final int ic_launcher=0x7f020011;
+        public static final int india=0x7f020012;
+        public static final int j=0x7f020013;
+        public static final int joker=0x7f020014;
+        public static final int k=0x7f020015;
+        public static final int kite=0x7f020016;
+        public static final int l=0x7f020017;
+        public static final int lion=0x7f020018;
+        public static final int m=0x7f020019;
+        public static final int monkey=0x7f02001a;
+        public static final int n=0x7f02001b;
+        public static final int nest=0x7f02001c;
+        public static final int o=0x7f02001d;
+        public static final int orange=0x7f02001e;
+        public static final int p=0x7f02001f;
+        public static final int parrot=0x7f020020;
+        public static final int q=0x7f020021;
+        public static final int queen=0x7f020022;
+        public static final int r=0x7f020023;
+        public static final int rat=0x7f020024;
+        public static final int rounded=0x7f020025;
+        public static final int s=0x7f020026;
+        public static final int snake=0x7f020027;
+        public static final int t=0x7f020028;
+        public static final int train=0x7f020029;
+        public static final int u=0x7f02002a;
+        public static final int umbrella=0x7f02002b;
+        public static final int v=0x7f02002c;
+        public static final int van=0x7f02002d;
+        public static final int w=0x7f02002e;
+        public static final int watch=0x7f02002f;
+        public static final int x=0x7f020030;
+        public static final int xmas=0x7f020031;
+        public static final int y=0x7f020032;
+        public static final int yak=0x7f020033;
+        public static final int z=0x7f020034;
+        public static final int zebra=0x7f020035;
     }
     public static final class id {
         public static final int action_settings=0x7f0a0001;
@@ -47,7 +97,6 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f070001;
         public static final int app_name=0x7f070000;
-        public static final int hello_world=0x7f070002;
     }
     public static final class style {
         /** 
--- src/com/jaaga/learnnplay/MainActivity.java
+++ src/com/jaaga/learnnplay/MainActivity.java
@@ -3,48 +3,60 @@ package com.jaaga.learnnplay;
 import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.drawable.AnimationDrawable;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
-import android.view.animation.Animation;
 import android.widget.ImageButton;
 
 public class MainActivity extends Activity {
 
 	ImageButton imagebutton;
-	MediaPlayer a;
-	MediaPlayer apple;
+	MediaPlayer[] media = new MediaPlayer[26];
+	private static int[] photos = new int[]{R.drawable.a,R.drawable.apple,R.drawable.b,
+		R.drawable.ball,R.drawable.c,R.drawable.cat,R.drawable.d,R.drawable.dog,R.drawable.e,
+		R.drawable.elephant,R.drawable.f,R.drawable.fan,R.drawable.g,R.drawable.goat,
+		R.drawable.h,R.drawable.hen,R.drawable.i,R.drawable.india,R.drawable.j,
+		R.drawable.joker,R.drawable.k,R.drawable.kite,R.drawable.l,R.drawable.lion,
+		R.drawable.m,R.drawable.monkey,R.drawable.n,R.drawable.nest,R.drawable.o,
+		R.drawable.orange,R.drawable.p,R.drawable.parrot,R.drawable.q,R.drawable.queen,
+		R.drawable.r,R.drawable.rat,R.drawable.s,R.drawable.snake,R.drawable.t,
+		R.drawable.train,R.drawable.u,R.drawable.umbrella,R.drawable.v,R.drawable.van,
+		R.drawable.w,R.drawable.watch,R.drawable.x,R.drawable.xmas,R.drawable.y,
+		R.drawable.yak,R.drawable.z,R.drawable.zebra};
+	private static int click;
+	private static int[] song=new int[]{R.raw.a,R.raw.abc};
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		imagebutton=(ImageButton) findViewById(R.id.imageButton1);
-		a=MediaPlayer.create(this, R.raw.a);
-		apple=MediaPlayer.create(this, R.raw.abc);
-		a.start();	
+		click=0;
+		
+		imagebutton = (ImageButton) findViewById(R.id.imageButton1);
+		
+		media[1] = MediaPlayer.create(this, song[0]);
+		media[2] = MediaPlayer.create(this, song[1]);
+		media[1].start();	
 		imagebutton.setOnClickListener(new View.OnClickListener() {
 			
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				a.stop();
-				apple.start();
-				Bitmap image = BitmapFactory.decodeResource(getResources(),R.drawable.apple);  
-				imagebutton.setImageBitmap(image);  
-				imagebutton.setMinimumWidth(image.getWidth());  
-				imagebutton.setMinimumHeight(image.getHeight());
+				click++;
+				Bitmap image = BitmapFactory.decodeResource(getResources(),photos[click]);  
+				imagebutton.setImageBitmap(image);
+				if(click % 2 == 1)
+				{
+					media[1].stop();
+					media[2].start();
+				}
+				else {
+					media[2].stop();
+				}
 			}
 		});
     }
-	
-	/*private void animated()
-	{
-		imagebutton.setImageResource(R.anim.move);
-		AnimationDrawable animdraw=(AnimationDrawable) imagebutton.getDrawable();
-	    animdraw.start();
-	}
-*/
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -52,12 +64,4 @@ public class MainActivity extends Activity {
 		return true;
 	}
 	
-	public void changes(View v)
-	{
-		//imagebutton.setImageResource(R.drawable.apple);
-		Bitmap image = BitmapFactory.decodeResource(getResources(),R.drawable.apple);  
-		  imagebutton.setImageBitmap(image);  
-		  imagebutton.setMinimumWidth(image.getWidth());  
-		  imagebutton.setMinimumHeight(image.getHeight());
-	}
 }
