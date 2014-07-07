1c77b2a30c9ab53ebb12c9d88a7eb8ff9c9dcd1b
Arpit <arpitagrawal910@gmail.com>
2014-07-08 00:14:19 +0530

    shake to clear

--- src/com/jaaga/learnnplay/PaintMe.java
+++ src/com/jaaga/learnnplay/PaintMe.java
@@ -1,19 +1,26 @@
 package com.jaaga.learnnplay;
 
 import android.app.Activity;
-import android.content.SharedPreferences;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Point;
+import android.graphics.PorterDuff.Mode;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
+import android.text.InputType;
+import android.util.Log;
 import android.view.Display;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
+import android.widget.EditText;
+
+import com.jaaga.learnnplay.ShakeDetector.OnShakeListener;
 
 public class PaintMe extends Activity {
 
@@ -22,73 +29,107 @@ public class PaintMe extends Activity {
 	private Canvas c;
 	private static final String TAG = "MainActivity";
 	private static String mAlphaName = "";
-	
+	private SensorManager mSensorManager;
+	private Sensor mAccelerometer;
+	private ShakeDetector mShakeDetector;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-		mAlphaName = prefs.getString("name", "no name");
 		
+
+		mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+		mAccelerometer = mSensorManager
+				.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+		
+		alertdialog();
+
 		Display display = getWindowManager().getDefaultDisplay();
-        Point size = new Point();
-        display.getSize(size);
-        final int width = size.x;
-        final int height = size.y;
-        
-        mBitmap = Bitmap.createBitmap(width, height,Bitmap.Config.ARGB_8888);
-        c = new Canvas(mBitmap);
-        p = new Paint();
+		Point size = new Point();
+		display.getSize(size);
+		final int width = size.x;
+		final int height = size.y;
 
-        View view = new View(this){
+		mBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+		c = new Canvas(mBitmap);
+		p = new Paint();
+		
+		mShakeDetector = new ShakeDetector(new OnShakeListener() {
 
-			protected void onDraw(Canvas canvas) 
-        	{	
-        		canvas.drawBitmap(mBitmap, 0, 0, null);
-        	
-        		p.setTextSize(width/1.2f);
-        		p.setColor(0xff0000ff);
-        		canvas.drawText(mAlphaName,width/5, height/1.7f, p);
-        		
-        		postInvalidateDelayed(0);
-        	}
-        };
-        
-        setContentView(view);
-        
-        view.setOnTouchListener(new OnTouchListener() {
-			
 			@Override
-			public boolean onTouch(View v, MotionEvent event) {
-		        float Pointx = event.getX();
-		        float Pointy = event.getY();
-		        
-		        p.setColor(0xff0000ff);
-		        c.drawCircle(Pointx, Pointy, 10, p);
-		        return true;
+			public void onShake() {
+				c.drawColor(0, Mode.CLEAR);
+				c.drawText(mAlphaName, width / 5, height / 1.7f, p);
+				Log.d(TAG, "shake");
 			}
 		});
 
+		final View view = new View(this) {
+
+			protected void onDraw(Canvas canvas) {
+				canvas.drawBitmap(mBitmap, 0, 0, null);
+
+			    p.setTextSize(60);
+				p.setColor(0xff000000);
+				canvas.drawText("Shake to Clear", width/3.5f, 50, p);
+			
+				p.setColor(0xff0000ff);
+				p.setTextSize(width / 1.2f);
+				canvas.drawText(mAlphaName, width / 5, height / 1.7f, p);
+				
+				postInvalidateDelayed(0);
+			}
+		};
+
+		setContentView(view);
+
+		view.setOnTouchListener(new OnTouchListener() {
+
+			@Override
+			public boolean onTouch(final View v, MotionEvent event) {
+				float Pointx = event.getX();
+				float Pointy = event.getY();
+
+				p.setColor(0xff0000ff);
+				c.drawCircle(Pointx, Pointy, 10, p);
+
+				return true;
+			}
+		});
+	}
+	
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+		mSensorManager.registerListener(mShakeDetector, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        
+	}
+	
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		mSensorManager.unregisterListener(mShakeDetector);
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
+	private void alertdialog() {
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setTitle("Set the Alphabet/Number");
+		builder.setMessage("Enter the name of Alphabet/Number to displayed");
+		final EditText input = new EditText(this);
+		input.setInputType(InputType.TYPE_CLASS_TEXT);
+		builder.setView(input);
+		builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
 
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.paint_me, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
+			@Override
+			public void onClick(DialogInterface arg0, int arg1) {
+				mAlphaName = input.getText().toString();
+			}
+		});
+		AlertDialog dialog = builder.create();
+		dialog.show();
 	}
 
 }
