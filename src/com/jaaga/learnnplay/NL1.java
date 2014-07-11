fb78c855d1fbb59e86d71f54eadfbbb2e64bd5b9
Arpit <arpitagrawal910@gmail.com>
2014-07-11 05:40:11 +0530

    number sound

--- src/com/jaaga/learnnplay/NL1.java
+++ src/com/jaaga/learnnplay/NL1.java
@@ -11,20 +11,25 @@ import android.graphics.Point;
 import android.media.MediaPlayer;
 import android.media.MediaPlayer.OnCompletionListener;
 import android.os.Bundle;
-import android.os.SystemClock;
 import android.view.Display;
 import android.view.Menu;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class NL1 extends Activity {
 
+	MediaPlayer[] media = new MediaPlayer[10];
 	private static int click = 0;
 
 	private static int[] photos = new int[] { R.drawable.n1, R.drawable.n2,
 			R.drawable.n3, R.drawable.n4, R.drawable.n5, R.drawable.n6,
-			R.drawable.n7, R.drawable.n8, R.drawable.n9, R.drawable.n10 };
+			R.drawable.n7, R.drawable.n8, R.drawable.n9, R.drawable.n10};
+
+	private static int[] song = new int[] { R.raw.n1, R.raw.n2, R.raw.n3, R.raw.n4,
+			R.raw.n5, R.raw.n6, R.raw.n7, R.raw.n8, R.raw.n9, R.raw.n10};
 
 	private Bitmap mBitmap;
 	private Bitmap[] mAlpha = new Bitmap[10];
@@ -39,13 +44,16 @@ public class NL1 extends Activity {
 	private Canvas c;
 	private static final String TAG = "MainActivity";
 	private boolean mTouching;
+	private Button btn;
 	private static int next = 0;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		//media[click] = MediaPlayer.create(this, song[click]);
+		Toast.makeText(this, "Tab to screen 3 times for next Alphabet", Toast.LENGTH_SHORT).show();
+		media[click] = MediaPlayer.create(this, song[click]);
+		btn = new Button(this);
 
 		Display display = getWindowManager().getDefaultDisplay();
 		Point size = new Point();
@@ -75,51 +83,71 @@ public class NL1 extends Activity {
 				canvas.scale(ScaleX, ScaleY);
 				canvas.drawBitmap(mBG, 0, 0, null);
 
+				btn.draw(canvas);
 				canvas.restore();
 
 				canvas.translate(x, y);
 
-				if (mTouching && (next % 3 == 0)) 
-				{
+				if (mTouching && (next % 3 == 0)) {
 					vx = 1;
 					vy = 1;
 					x = 0;
 					y = 0;
+					media[click].stop();
+					media[click].release();
 					click++;
 					if (click <= 9) {
-						
+						media[click] = MediaPlayer.create(
+								getApplicationContext(), song[click]);
 						
 						canvas.drawBitmap(mAlpha[click], 0, 0, null);
-						
+						media[click].start();
+						media[click]
+								.setOnCompletionListener(new OnCompletionListener() {
+
+									@Override
+									public void onCompletion(MediaPlayer mp) {
+										mp.reset();
+									}
+								});
 					} else {
 						click = 0;
-						
+						media[click] = MediaPlayer.create(
+								getApplicationContext(), song[click]);
 					}
 				} else {
 					canvas.drawBitmap(mAlpha[click], 0, 0, null);
-					
-				}
 
+					media[click]
+							.setOnCompletionListener(new OnCompletionListener() {
+
+								@Override
+								public void onCompletion(MediaPlayer mp) {
+									mp.reset();
+
+								}
+							});
+					media[click].start();
+				}
+				
 				Random rw = new Random();
 				final int rWidth = rw.nextInt(3);
 
 				Random rh = new Random();
 				final int rHeight = rh.nextInt(3);
-				
+
 				x = x + vx;
 				y = y + vy;
 
 				if ((y + 2 * mAlphaHheight[0] - 1 >= this.getHeight())
 						|| (y <= -1)) {
 					vy = -vy;
-				} 
-				else {vy = vy + rHeight * 0.4f;}
+				} else {vy = vy + rHeight *0.4f;}
 
 				if ((x + 2 * mAlphaHwidth[0] + 1 >= this.getWidth())
 						|| (x <= -1)) {
 					vx = -vx;
-				} 
-				else {vx = vx + rWidth * 0.5f;}
+				} else {vx = vx + rWidth *0.5f;}
 
 				postInvalidateDelayed(2);
 			}
@@ -130,7 +158,6 @@ public class NL1 extends Activity {
 
 			@Override
 			public boolean onTouch(View v, MotionEvent event) {
-				
 				int action = event.getAction();
 				if (action == MotionEvent.ACTION_DOWN) {
 					mTouching = true;
@@ -162,5 +189,4 @@ public class NL1 extends Activity {
 		getMenuInflater().inflate(R.menu.alphabets_word, menu);
 		return true;
 	}
-
 }
